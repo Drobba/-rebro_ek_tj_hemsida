3b5d4d42a126f74f844b4884875d56a97f40404e
Drobba <a.nelefelt@gmail.com>
2022-07-25 14:51:28 +0200

    commit

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li"); //Hämtar ALLA enskilda länkar i nav-links
-  //Toggle nav
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-  burger.addEventListener("click", () => {
-    //Toggle nav
-    nav.classList.toggle("nav-active");
-    //Animate links
-    navLinks.forEach((link, index) => {
-      //Index visar hur många links vi har.
-      if (link.style.animation) {
-        //Om links har animation sätt den till inget
-        link.style.animation = "";
-      } else {
-        //Annars adda animation
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.3
-        }s`;  
-      }
-    });
-    // Burger menu animation
-    burger.classList.toggle("toggle");
-  });
-};
-const app = () => {
-  navSlide();
-};
-app();
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
